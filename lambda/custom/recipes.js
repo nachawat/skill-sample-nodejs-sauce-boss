5d36ac5fca0979f05422a93dbe8530deab2dfdc3
Florian Hollandt <florian.hollandt@gmail.com>
2019-10-10 23:03:18 +0200

    Add placeholders for german recipe translations

--- lambda/custom/recipes.js
+++ lambda/custom/recipes.js
@@ -143,6 +143,62 @@ module.exports = {
         'SEC': {
             name: `Secreta`,
             instructions: `No hace falta que me dores la píldora, ni le eches guindas al pavo, la salsa está a buen resguardo.`
+        },
+    de: {
+        'HON': {
+            name: `Honig Senf`,
+            instructions: `Platzhalter für Honig-Senf-Sauce.`
+            // instructions: `To make honey mustard, mix mayonnaise, yellow mustard, Dijon mustard, honey, and lemon juice together in a bowl. Cover and chill in the refrigerator overnight.`
+        },
+        'BBQ': {
+            name: 'BBQ',
+            instructions: `Platzhalter für Barbeque-Sauce.`
+            // instructions: `To make BBQ sauce, combine brown sugar, ketchup, vinegar, and Worcestershire sauce in a blender. Season with salt, pepper, and cayenne pepper. Blend until smooth.`
+        },
+        'RAN': {
+            name: `Ranch`,
+            instructions: `Platzhalter für Ranch-Sauce.`
+            // instructions: `For ranch dressing, whisk together mayonnaise, sour cream, chives, parsley, dill, garlic powder, onion powder, salt and pepper in a large bowl. Cover and refrigerate for 30 minutes before serving.`
+        },
+        'CAE': {
+            name: `Caesar`,
+            instructions: `Platzhalter für Caesar-Sauce.`
+            // instructions: `For Caesar dressing, combine lemon juice, vinegar, water, shredded parmesan cheese, Dijon mustard, garlic powder and pepper in a jar. Cover with a lid and shake well. Refrigerate until ready to use.`
+        },
+        'WOR': {
+            name: `Worcestershire`,
+            instructions: `Platzhalter für Worcestershire-Sauce.`
+            // instructions: `To make Worcestershire sauce, combine apple cider vinegar, water, soy sauce, brown sugar, mustard powder, onion powder, garlic powder, ground cinnamon, and a pinch of black pepper together in a saucepan; bring to a boil and cook until fragrant, about 45 seconds, then cool to room temperature.`
+        },
+        'THO': {
+            name: `Thousand Island`,
+            instructions: `Platzhalter für Thousand-Island-Sauce.`
+            // instructions: `For Thousand Island dressing, mix together mayonnaise, ketchup, sweet pickle relish, salt and pepper in a small bowl until thoroughly combined. Chill and serve.`
+        },
+        'PES': {
+            name: `Pesto`,
+            instructions: `Platzhalter für Pesto-Sauce.`
+            // instructions: `To make pesto, combine basil, garlic, Parmesan cheese, olive oil, and pine nuts in a food processor or blender. Blend to a smooth paste. Add parsley if desired.`
+        },
+        'TAR': {
+            name: `Tartar`,
+            instructions: `Platzhalter für Tartar-Sauce.`
+            // instructions: `For tartar sauce, combine mayonnaise, chopped onion, sweet pickle relish, salt and pepper in a medium bowl. Mix well and let stand for at least 10 minutes before serving.`
+        },
+        'PIZ': {
+            name: `Pizza`,
+            instructions: `Platzhalter für Pizza-Sauce.`
+            // instructions: `To make pizza sauce, mix together tomato sauce and tomato paste in a medium bowl until smooth. Stir in oregano, dried minced garlic and paprika.`
+        },
+        'CRA': {
+            name: `Cranberry`,
+            instructions: `Platzhalter für Cranberry-Sauce.`
+            // instructions: `For cranberry sauce, dissolve sugar in orange juice in a saucepan over medium heat. Stir in cranberries and cook until they start to pop. Remove from heat and transfer the sauce to a bowl before serving.`
+        },
+        'SEC': {
+            name: `Secret`,
+            instructions: `Das Rezept für die geheime Sauce ist bei mir sicher. Du brauchst gar nicht weiter versuchen, es aus mir raus zu quetschen!`
         }
+    },
     }
 }
